4b243364ce9e3130c5300f8c1e8e884386439334
James Richmond <devenger@gmail.com>
2019-06-04 22:19:21 +0100

    Updates

---
+++ component---src-pages-index-js-51fa17e96c3dae450942.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{140:function(e,n,t){"use strict";t.r(n);var o=t(0),r=t.n(o);n.default=function(){return r.a.createElement("div",null,"Hello lost reader.")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-51fa17e96c3dae450942.js.map
